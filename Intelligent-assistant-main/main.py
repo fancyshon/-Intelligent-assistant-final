4c550a55fb6603b2aa7e5dd06391317f0cad2b03
KJay221 <k20002216408@gmail.com>
2022-04-08 20:35:25 +0800

    delete some file

--- Intelligent-assistant-main/main.py
+++ Intelligent-assistant-main/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
 from config import Config
 
-from routers import home, server, inter, predict
-
-from fakedata import add_fakedata
-
+from routers import home, server, predict
+from routers import inter
 
 
 app = FastAPI()
@@ -13,9 +11,6 @@ app.include_router(server.router)
 app.include_router(inter.router)
 app.include_router(predict.router)
 
-
-if Config.FAKE_DATA == "true":
-    add_fakedata()
 
 if __name__ == '__main__':
     import uvicorn
