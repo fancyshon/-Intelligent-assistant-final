f3bc4229d98f90acc7a9caa7aa02b4429fa3f12f
KJay221 <k20002216408@gmail.com>
2022-04-08 20:40:54 +0800

    add middleware

--- Intelligent-assistant-main/main.py
+++ Intelligent-assistant-main/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config import Config
 
 from routers import home, server, predict
@@ -6,6 +7,15 @@ from routers import inter
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(home.router)
 app.include_router(server.router)
 app.include_router(inter.router)
